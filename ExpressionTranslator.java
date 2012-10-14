c08ad258e6b9e0b4b32e35d9e9c57eb5cf9e0d9f
Sokolov Ruslan <susak-1@yandex.ru>
2012-10-14 20:06:45 +0400

    Second commit

--- ExpressionTranslator.java
+++ ExpressionTranslator.java
@@ -10,10 +10,16 @@ public class ExpressionTranslator {
 	String answer = "";
 	
 	String getTranslation(String expression) throws IOException {
+		String[] subStrings = expression.split(" ");
+		String temp = subStrings[0];
 
+		for (int i = 1; i < subStrings.length; i++) {
+			temp += "+" + subStrings[i];
+		}
+		
 		url = new URL(
 				"http://translate.yandex.ru/tr.json/translate?lang=en-ru&text="
-						+ expression);
+						+ temp);
 		BufferedReader in = new BufferedReader(new InputStreamReader(
 				url.openStream(), "UTF-8"));
 
